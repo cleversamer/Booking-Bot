49963e503ed684f2dcbabe944eb52ae6c83480bb
Samer A.⚡ <73291969+ssadawi@users.noreply.github.com>
2022-02-23 17:12:11 +0200

    Delete config directory

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-data = []
-
-with open('config/config.txt', 'r') as file:
-    for line in file.readlines():
-        data.append(line)
-
-with open('config/config.py', 'w') as file:
-    file.writelines(data)
